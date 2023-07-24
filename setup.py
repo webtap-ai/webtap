08b4244868b6789bb780279628da048562fbb8c0
Stefano <stefanopochet@gmail.com>
2023-07-24 18:42:28 +0200

    Llibrary skeleton setup

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="webtap",
+    version="0.1",
+    packages=find_packages(),
+    author="Stefano Pochet",
+    author_email="stefanopochet@gmail.com",
+    description="Accessing the Web's Deep Data Aquifers with a Simple Tap",
+    url="https://github.com/webtap-ai/webtap.git",  # Substitute with the actual URL
+)
