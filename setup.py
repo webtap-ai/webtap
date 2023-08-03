de0ff635887a381ef85c611b9389c0755baffa66
Stefano <stefanopochet@gmail.com>
2023-08-03 18:53:08 +0200

    Added package data

--- setup.py
+++ setup.py
@@ -2,8 +2,10 @@ from setuptools import setup, find_packages
 
 setup(
     name="webtap",
-    version="0.1.3",
+    version="0.1.4",
     packages=find_packages(),
+    include_package_data=True,
+    package_data={'': ['*.json', '*.html', '*.css', '*.js', '*.png', '*.jpg', '*.jpeg', '*.gif', '*.svg', '*.ico']},
     author="Stefano Pochet",
     author_email="stefanopochet@gmail.com",
     description="Accessing the Web's Deep Data Aquifers with a Simple Tap",
