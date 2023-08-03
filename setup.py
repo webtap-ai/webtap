8c753fcbab7bafb45784b458bdf3a70caf68c6a7
Stefano <stefanopochet@gmail.com>
2023-08-03 19:50:56 +0200

    upgraded version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="webtap",
-    version="0.1.5.1",
+    version="0.1.5.2",
     packages=find_packages(),
     include_package_data=True,
     package_data={'': ['*.json', '*.txt']},
