305aa72c425f45ac9dbb1997fb6a15823e24ab6a
Stefano <stefanopochet@gmail.com>
2023-09-19 17:20:53 +0200

    Upgraded version number

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="webtap",
-    version="0.2.7",
+    version="0.3.3",
     packages=find_packages(),
     install_requires=[
         'aiohttp==3.8.3',
