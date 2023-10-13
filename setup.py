eece89084b5f5fd14442aa3649e2ff18f9d26944
klokt-valg <20753260+klokt-valg@users.noreply.github.com>
2023-10-13 11:03:51 -0400

    version upgrade 0.3.17

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="webtap",
-    version="0.3.16",
+    version="0.3.17",
     packages=find_packages(),
     install_requires=[
         'aiohttp==3.8.3',
