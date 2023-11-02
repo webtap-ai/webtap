9ad87f42793f0e8c036514d7d6ddda6d034370e0
alpha8eta <117678345+alpha8eta@users.noreply.github.com>
2023-11-02 13:27:35 -0400

    remove your_env

--- your_env/lib/python3.11/site-packages/apify_client/__init__.py
+++
@@ -1,7 +0,0 @@
-from importlib import metadata
-
-from .client import ApifyClient, ApifyClientAsync
-
-__version__ = metadata.version('apify-client')
-
-__all__ = ['ApifyClient', 'ApifyClientAsync', '__version__']
