6055e29f51f2261fd0597c599343f883e4e5828d
Stefano <stefanopochet@gmail.com>
2023-11-12 21:44:16 +0100

    minor fix

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="webtap",
-    version="0.3.35",
+    version="0.3.36",
     packages=find_packages(),
     install_requires=[
         "aiohttp==3.8.3",
