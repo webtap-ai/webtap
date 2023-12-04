dd6073cfa3bfde50aa3f10ebae5bd6bf6e8c4d40
Stefano <stefanopochet@gmail.com>
2023-12-04 17:35:19 +0100

    remove facebook tap

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="webtap",
-    version="0.3.47",
+    version="0.3.48",
     packages=find_packages(),
     install_requires=[
         "aiohttp==3.8.3",
