2730d4e4b1b5ae5fc3165af6e37749072d2b67cc
Stefano <stefanopochet@gmail.com>
2023-12-05 11:12:14 +0100

    increment version number

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="webtap",
-    version="0.3.50",
+    version="0.3.52",
     packages=find_packages(),
     install_requires=[
         "aiohttp==3.8.3",
