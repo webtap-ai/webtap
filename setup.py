0e894288cd13eefa202764c04e2e694c83e5ae22
Stefano <stefanopochet@gmail.com>
2024-08-22 10:25:03 +0200

    chore(release): v.0.4.4 (#20)

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="webtap",
-    version="0.4.3",
+    version="0.4.4",
     packages=find_packages(),
     install_requires=[
         "aiohttp==3.8.3",
